529daed21465c14bd7f03483a4b83142def0de6e
reza <mr.rezakarimi99@gmail.com>
2023-04-01 11:30:03 +0330

    fix some issue

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# for use your laravel application 
+
+``` bash
+sudo wget -O - https://raw.githubusercontent.com/mrrezakarimi99/make_project/main/deploy.sh | sudo bash
+```
+
+
--- deploy.sh
+++ deploy.sh
@@ -2,32 +2,32 @@
 
 #Global Functions
 
-infoMessage(){
+infoMessage() {
   echo -e "\e[34m$1\e[0m"
 }
 
-successMessage(){
+successMessage() {
   echo -e "\e[32m$1\e[0m"
 }
 
-errorMessage(){
+errorMessage() {
   echo -e "\e[31m$1\e[0m"
 }
 
 #end Global Functions
 #Main Functions
 
-update(){
+update() {
   echo "update ..."
   apt update
 }
 
-upgrade(){
+upgrade() {
   echo "upgrade ..."
   apt upgrade -y
 }
 
-installRequired(){
+installRequired() {
   echo "installing required packages ..."
   apt install -y git curl gcc g++ make apt-transport-https ca-certificates \
     gnupg lsb-release dirmngr software-properties-common \
@@ -35,41 +35,40 @@ installRequired(){
 }
 
 getInstallation() {
-    infoMessage "do you want to install $1 (y/n)"
-    read -r i
-    if [ "$i" = "y" ];
-    then
-      install_"$1"
-    fi
+  infoMessage "do you want to install $1 (y/n)"
+  read -r i
+  if [ "$i" = "y" ]; then
+    install_"$1"
+  fi
 }
 
 install_nginx() {
-    if [ -x "$(command -v nginx)" ]; then
-        errorMessage 'Error: nginx is already installed.' >&2
-    else
-      infoMessage "installing nginx ..."
-      apt install -y nginx
-      systemctl start nginx
-      systemctl enable nginx
-      ufw allow 'Nginx Full'
-      successMessage "nginx installed successfully"
-    fi
+  if [ -x "$(command -v nginx)" ]; then
+    errorMessage 'Error: nginx is already installed.' >&2
+  else
+    infoMessage "installing nginx ..."
+    apt install -y nginx
+    systemctl start nginx
+    systemctl enable nginx
+    ufw allow 'Nginx Full'
+    successMessage "nginx installed successfully"
+  fi
 }
 
 install_mysql() {
-    if [ -x "$(command -v mysql)" ]; then
-        errorMessage 'Error: mysql is already installed.' >&2
-    else
-      infoMessage "installing mysql ..."
-      apt install -y mysql-server
-      systemctl start mysql
-      systemctl enable mysql
-      mysql_secure_installation
-      successMessage "mysql installed successfully"
-    fi
+  if [ -x "$(command -v mysql)" ]; then
+    errorMessage 'Error: mysql is already installed.' >&2
+  else
+    infoMessage "installing mysql ..."
+    apt install -y mysql-server
+    systemctl start mysql
+    systemctl enable mysql
+    mysql_secure_installation
+    successMessage "mysql installed successfully"
+  fi
 }
 
-mysql_secure_installation(){
+mysql_secure_installation() {
   infoMessage "mysql_secure_installation ..."
   infoMessage "please enter mysql root password"
   read -r password
@@ -82,62 +81,62 @@ EOF
 }
 
 install_php() {
-    if [ -x "$(command -v php)" ]; then
-        errorMessage 'Error: php is already installed.' >&2
-    else
-      infoMessage "installing php ..."
-      apt install -y php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
-      systemctl start php-fpm
-      systemctl enable php-fpm
-      successMessage "php installed successfully"
-    fi
+  if [ -x "$(command -v php)" ]; then
+    errorMessage 'Error: php is already installed.' >&2
+  else
+    infoMessage "installing php ..."
+    apt install -y php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+    systemctl start php-fpm
+    systemctl enable php-fpm
+    successMessage "php installed successfully"
+  fi
 }
 
 install_docker() {
-    if [ -x "$(command -v docker)" ]; then
-        errorMessage 'Error: docker is already installed.' >&2
-    else
-      infoMessage "installing docker ..."
-      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-      add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-      infoMessage "updating ..."
-      apt update
-      apt-cache policy docker-ce
-      apt install -y docker-ce
-      systemctl status docker
-      usermod -aG docker ${USER}
-      su - ${USER}
-      id -nG
-      successMessage "docker installed successfully"
-    fi
+  if [ -x "$(command -v docker)" ]; then
+    errorMessage 'Error: docker is already installed.' >&2
+  else
+    infoMessage "installing docker ..."
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    infoMessage "updating ..."
+    apt update
+    apt-cache policy docker-ce
+    apt install -y docker-ce
+    systemctl status docker
+    usermod -aG docker ${USER}
+    su - ${USER}
+    id -nG
+    successMessage "docker installed successfully"
+  fi
 }
 
 install_docker_compose() {
-    if [ -x "$(command -v docker-compose)" ]; then
-        errorMessage 'Error: docker-compose is already installed.' >&2
-    else
-      infoMessage "installing docker-compose ..."
-      curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-      chmod +x /usr/local/bin/docker-compose
-      ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-      docker-compose --version
-      successMessage "docker-compose installed successfully"
-    fi
+  if [ -x "$(command -v docker-compose)" ]; then
+    errorMessage 'Error: docker-compose is already installed.' >&2
+  else
+    infoMessage "installing docker-compose ..."
+    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    docker-compose --version
+    successMessage "docker-compose installed successfully"
+  fi
 }
 
 install_composer() {
-    if [ -x "$(command -v composer)" ]; then
-        errorMessage 'Error: composer is already installed.' >&2
-    else
-      infoMessage "installing composer ..."
-      curl -sS https://getcomposer.org/installer | php
-      mv composer.phar /usr/local/bin/composer
-      chmod +x /usr/local/bin/composer
-      successMessage "composer installed successfully"
-    fi
+  if [ -x "$(command -v composer)" ]; then
+    errorMessage 'Error: composer is already installed.' >&2
+  else
+    infoMessage "installing composer ..."
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+    chmod +x /usr/local/bin/composer
+    successMessage "composer installed successfully"
+  fi
 }
 
-createDataBase(){
+createDataBase() {
   infoMessage "please enter mysql root password"
   read -r password
   mysql -u root -p"$password" <<EOF
@@ -164,26 +163,22 @@ infoMessage "     /____/\____/\____/_/ |_|\____/_____/___/_/ |_/\____/_/  |_|   
 infoMessage "======================================================================="
 infoMessage "Do you want to update and upgrade (y/n)"
 read -r i
-if [ "$i" = "y" ];
-then
+if [ "$i" = "y" ]; then
   update
   upgrade
 fi
 
 infoMessage "Do you want to install required packages (y/n)"
 read -r i
-if [ "$i" = "y" ];
-then
+if [ "$i" = "y" ]; then
   installRequired
 fi
 
 infoMessage "Do you want to install nginx, mysql, php, docker and docker-compose (y/n)"
 read -r i
-if [ "$i" = "y" ];
-then
-  for i in nginx mysql php docker docker_compose
-  do
-      getInstallation $i
+if [ "$i" = "y" ]; then
+  for i in nginx mysql php docker docker_compose; do
+    getInstallation $i
   done
 fi
 
@@ -191,8 +186,8 @@ fi
 infoMessage "Please enter directory project from /home/$USER (for example: project/foo)"
 read -r -e -p "Please enter the path to the directory you want to use: " -i "/home/$USER"
 if [ ! -d "$REPLY" ]; then
-    infoMessage "Creating directory $REPLY"
-    mkdir -p "$REPLY"
+  infoMessage "Creating directory $REPLY"
+  mkdir -p "$REPLY"
 fi
 cd "$REPLY" || exit
 
@@ -234,20 +229,20 @@ successMessage "env file created successfully"
 infoMessage "Do you want to setup nginx? (y/n)"
 read -r nginx
 if [ "$nginx" = "y" ]; then
-    mv nginx.conf.example "$app_name".conf
-    sed -i "s/__DOMAIN__/$app_name/g" "$app_name".conf
-    finalPath="/home/$USER/$directory/public"
-    sed -i "s/__PATH__/$finalPath/g" "$app_name".conf
-    cp "$app_name".conf /etc/nginx/sites-available
-    ln -s /etc/nginx/sites-available/"$app_name".conf /etc/nginx/sites-enabled/
-    rm "$app_name".conf
-    successMessage "nginx config file created successfully"
-    infoMessage "Do you want to restart nginx? (y/n)"
-    read -r restart_nginx
-    if [ "$restart_nginx" = "y" ]; then
-        systemctl restart nginx
-        successMessage "nginx restarted successfully"
-    fi
+  mv nginx.conf.example "$app_name".conf
+  sed -i "s/__DOMAIN__/$app_name/g" "$app_name".conf
+  finalPath="/home/$USER/$directory/public"
+  sed -i "s/__PATH__/$finalPath/g" "$app_name".conf
+  cp "$app_name".conf /etc/nginx/sites-available
+  ln -s /etc/nginx/sites-available/"$app_name".conf /etc/nginx/sites-enabled/
+  rm "$app_name".conf
+  successMessage "nginx config file created successfully"
+  infoMessage "Do you want to restart nginx? (y/n)"
+  read -r restart_nginx
+  if [ "$restart_nginx" = "y" ]; then
+    systemctl restart nginx
+    successMessage "nginx restarted successfully"
+  fi
 fi
 
 #sudo wget -O - https://raw.githubusercontent.com/mrrezakarimi99/make_project/main/deploy.sh | sudo bash
